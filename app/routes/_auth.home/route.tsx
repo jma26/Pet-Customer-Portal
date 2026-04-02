289fd15b00a1ae92af36a0d7a55af07f762bc127
Jesse Ma <jma26code@gmail.com>
2026-04-02 16:04:35 -0700

    Dashboard Home route now loads user's pet info

--- app/routes/_auth.home/route.tsx
+++ app/routes/_auth.home/route.tsx
@@ -1,6 +1,38 @@
 import Placeholder from '~/assets/camera-placeholder.svg';
+import type { Route } from './+types/route';
 
-export default function Home() {
+interface Pet {
+  id: string,
+  user_id: string,
+  created_at?: Date,
+  name: string,
+  age: number,
+  breed: string,
+  type: string,
+  avatar_path: string
+}
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const { createSupabaseServerClient } = await import('~/lib/supabase.server');
+  const { supabase } = createSupabaseServerClient(request);
+  // Retrieve pets from database
+  const { data, error } = await supabase.from('pets').select();
+  // Retrieve avatar for each pet
+  const pets = data?.map((pet) => {
+    const { data } = supabase.storage.from('pet-photos').getPublicUrl(`${pet.avatar_path}`);
+    return {
+      ...pet,
+      avatar_path: data.publicUrl ?? null
+    }
+  })
+  console.log('What are the pets', pets);
+  return { pets, error };
+}
+
+export default function Home({ loaderData }: { loaderData: { pets: Pet[], error: unknown }}) {
+  const { pets, error } = loaderData
+  console.log('There is an error', error);
+
   return (
     <>
       <h1>Home</h1>
@@ -19,7 +51,7 @@ export default function Home() {
             <div className="card bg-base-100 card-sm min-w-0 shadow-sm">
               <div className="card-body">
                 <h3 className="card-title font-normal text-sm ">Pets</h3>
-                <p className="text-lg font-medium self-center">3</p>
+                <p className="text-lg font-medium self-center">{pets.length}</p>
               </div>
             </div>
             <div className="card bg-base-100 card-sm min-w-0 shadow-sm">
@@ -40,24 +72,39 @@ export default function Home() {
         <section className="flex flex-col gap-2">
           <h2 className="font-semibold text-lg">Your Pets</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            <div className="card card-side bg-base-100 card-sm p-4 shadow-sm">
-              <div className="avatar w-24">
-                <div className="avatar aspect-square rounded-full">
-                  <img 
-                    className="rounded-full" 
-                    src={Placeholder} 
-                    alt="Avatar" 
-                  />
+            { pets.length > 0 ? (
+              pets.map((pet) => {
+                return (
+                <div className="card card-side bg-base-100 card-sm p-4 shadow-sm" key={pet.id}>
+                  <div className="avatar w-24">
+                    <div className="avatar aspect-square rounded-full">
+                      <img 
+                        className="rounded-full" 
+                        src={pet.avatar_path} 
+                        alt={pet.name} 
+                      />
+                    </div>
+                  </div>
+                  <div className="card-body gap-1 w-full">
+                    <h3 className="card-title font-semibold text-normal">{pet.name}</h3>
+                    <div className="flex flex-col gap-1">
+                      <p className="text-xs">{pet.breed}</p>
+                      <p className="text-xs">{pet.age} years</p>
+                    </div>
+                  </div>
+                </div>
+                )
+              })
+            ) : (
+              <div className="card card-side bg-base-100 card-sm p-4 shadow-sm">
+                <div className="card-body gap-1 w-full">
+                  <h3 className="card-title font-semibold text-normal">You have no pets!</h3>
+                  <div className="flex flex-col gap-1">
+                    <p className="text-xs">Add a pet to get started.</p>
+                  </div>
                 </div>
               </div>
-              <div className="card-body gap-1 w-full">
-                <h3 className="card-title font-semibold text-normal">Earl Grey</h3>
-                <div className="flex flex-col gap-1">
-                  <p className="text-xs">Russian Blue Tabby</p>
-                  <p className="text-xs">3 years</p>
-                </div>
-              </div>
-            </div>
+            )}
           </div>
         </section>
       </div>
